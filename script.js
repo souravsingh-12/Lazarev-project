7a98878b1683a525520d5d24cbd59b434b4657fc
Sourav singh <singhsourav09024@gmail.com>
2025-08-31 01:05:51 +0530

    changes

--- script.js
+++ script.js
@@ -48,41 +48,58 @@ function Page3Animation() {
 }
 Page3Animation();
 
-function Page5Animation(){
-   let sections = document.querySelectorAll(".sec-right");
+function Page5Animation() {
+    let section1 = document.querySelectorAll(".sec-right");
+    let section2Left = document.querySelector("#sec-left");
+    let section2Right = document.querySelector("#sec-right");
 
-sections.forEach(function (elem) {
-    console.log(elem.childNodes);
+    section1.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1;
+            elem.childNodes[3].play();
+        });
 
-    elem.addEventListener("mouseenter", function () {
-        elem.childNodes[3].style.opacity = 1;
-        elem.childNodes[3].play();
-    });
-    elem.addEventListener("mouseleave", function () {
-        elem.childNodes[3].style.opacity = 0;
-        elem.childNodes[3].load();
-    });
-    elem.addEventListener("mouseenter", function () {
-        gsap.to(elem.childNodes[5], {
-            opacity: 1,
-            scale: 1,
+        elem.addEventListener("mouseleave", function () {
+            elem.childNodes[3].style.opacity = 0;
+            elem.childNodes[3].load();
+        });
+
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[5], {
+                opacity: 1,
+                scale: 1,
+            });
+        });
+
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[5], {
+                opacity: 0,
+                scale: 0,
+            });
+        });
+
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.childNodes[5], {
+                x: dets.x - elem.getBoundingClientRect().x - 70,
+                y: dets.y - elem.getBoundingClientRect().y - 75,
+            });
+        });
+
+        section2Left.addEventListener("mouseenter", function () {
+            
+        });
+
+        section2Left.addEventListener("mouseenter", function () {
+            console.log("hello");
         });
     });
-    elem.addEventListener("mouseleave", function () {
-        gsap.to(elem.childNodes[5], {
-            opacity: 0,
-            scale: 0,
-        });
-    });
-    elem.addEventListener("mousemove", function (dets) {
-        gsap.to(elem.childNodes[5], {
-            x: dets.x - elem.getBoundingClientRect().x - 70,
-            y: dets.y - elem.getBoundingClientRect().y - 75,
-        });
-    });
-});
 }
-Page5Animation()
+Page5Animation();
+
+
+
+
+
 // function check() {
 //     let a = document.querySelector(".center-play");
 //     a.addEventListener("mouseenter", function (elem) {
